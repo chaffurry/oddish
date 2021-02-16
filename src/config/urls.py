b1bb2fa0c7a139259bae16e20fd9344f52a8c5f4
chaffurry <58623871+chaffurry@users.noreply.github.com>
2021-02-16 12:12:47 +0800

    Update urls.py

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,6 @@
 import sys
 
-from src.config.definitions import CRAWL_MAX_PRICE_ITEM, CRAWL_MIN_PRICE_ITEM
+from src.config.definitions import CRAWL_MAX_PRICE_ITEM, CRAWL_MIN_PRICE_ITEM, CSGO
 #csgo & dota2    730 & 570
 BUFF_ROOT = 'https://buff.163.com/'
 STEAM_ROOT = 'https://steamcommunity.com/'
@@ -10,25 +10,40 @@ BUFF_HISTORY_PRICE_CNY = BUFF_ROOT + 'api/market/goods/price_history/buff?'
 
 
 def goods_root_url():
-    return BUFF_ROOT + 'market/?game=csgo#tab=selling&page_num=1'
-
+    if CSGO==1:
+        return BUFF_ROOT + 'market/?game=csgo#tab=selling&page_num=1'
+    else: 
+        return BUFF_ROOT + 'market/?game=dota2#tab=selling&page_num=1'
 
 def category_root_url(category):
-    return BUFF_GOODS + 'game=csgo&page_num=1&category=%s' % category
+    if CSGO==1:
+        return BUFF_GOODS + 'game=csgo&page_num=1&category=%s' % category
+    else:
+        return BUFF_GOODS + 'game=dota2&page_num=1&category=%s' % category
+    
 
 
 def category_page_url(page_num, category):
-    return BUFF_GOODS + 'game=csgo&page_num={}&category={}'.format(page_num, category)
-
+    if CSGO==1:
+        return BUFF_GOODS + 'game=csgo&page_num={}&category={}'.format(page_num, category)
+    else: 
+        return BUFF_GOODS + 'game=dota2&page_num={}&category={}'.format(page_num, category)
 
 def steam_price_history_url(item):
     """7 days history prices"""
     name = item.steam_url.rsplit('/', 1)[-1]
-    return STEAM_ROOT + 'market/pricehistory/?appid=730&market_hash_name={}'.format(name)
-
+    if CSGO==1:
+        return STEAM_ROOT + 'market/pricehistory/?appid=730&market_hash_name={}'.format(name)
+    else: 
+        return STEAM_ROOT + 'market/pricehistory/?appid=570&market_hash_name={}'.format(name)
+    
 
 def buff_price_history_url(item_id):
-    return BUFF_HISTORY_PRICE_CNY + 'game=csgo&goods_id={}&currency=CNY&days=7'.format(item_id)
+    if CSGO==1:
+        return BUFF_HISTORY_PRICE_CNY + 'game=csgo&goods_id={}&currency=CNY&days=7'.format(item_id)
+    else: 
+        return BUFF_HISTORY_PRICE_CNY + 'game=dota2&goods_id={}&currency=CNY&days=7'.format(item_id)
+    
 
 
 def goods_section_root_url(category):
@@ -38,8 +53,11 @@ def goods_section_root_url(category):
 
     So sys.maxsize here is used as page number in order to get all page number and item count.
     """
-
-    base = BUFF_GOODS + 'game=csgo&page_num={}&sort_by=price.asc&min_price={}&max_price={}' \
+    if CSGO==1:
+        base = BUFF_GOODS + 'game=csgo&page_num={}&sort_by=price.asc&min_price={}&max_price={}' \
+        .format(sys.maxsize, CRAWL_MIN_PRICE_ITEM, CRAWL_MAX_PRICE_ITEM)
+    else: 
+        base = BUFF_GOODS + 'game=dota2&page_num={}&sort_by=price.asc&min_price={}&max_price={}' \
         .format(sys.maxsize, CRAWL_MIN_PRICE_ITEM, CRAWL_MAX_PRICE_ITEM)
     if category is not None:
         base += '&category={}'.format(category)
@@ -49,8 +67,13 @@ def goods_section_root_url(category):
 
 def goods_section_page_url(category, page_num, page_size=20):
     # buff support page_size parameter, but the max value can only be 80
-    base = BUFF_GOODS + 'game=csgo&page_num={}&sort_by=price.desc&min_price={}&max_price={}&page_size={}' \
+    if CSGO==1:
+        base = BUFF_GOODS + 'game=csgo&page_num={}&sort_by=price.desc&min_price={}&max_price={}&page_size={}' \
         .format(page_num, CRAWL_MIN_PRICE_ITEM, CRAWL_MAX_PRICE_ITEM, page_size)
+    else: 
+        base = BUFF_GOODS + 'game=dota2&page_num={}&sort_by=price.desc&min_price={}&max_price={}&page_size={}' \
+        .format(page_num, CRAWL_MIN_PRICE_ITEM, CRAWL_MAX_PRICE_ITEM, page_size)
+        
     if category is not None:
         base += '&category={}'.format(category)
 
